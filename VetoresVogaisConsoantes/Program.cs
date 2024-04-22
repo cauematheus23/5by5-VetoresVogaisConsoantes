9e532713b3f539525547f05f8005fa9ea86f6a4c
cauematheus23 <cauematheus_23@hotmail.com>
2024-04-22 14:38:11 -0300

    Add project files.

---
+++ VetoresVogaisConsoantes/Program.cs
@@ -0,0 +1,25 @@
+﻿char[] carac = new char[10];
+for (int i = 0; i < 10; i++)
+{ 
+    Console.WriteLine($"Digite o {i}° caracter");
+    carac[i] = char.Parse(Console.ReadLine());
+    
+}
+Console.Write("Vogais: ");
+for (int j = 0; j < 10; j++)
+{
+    if (carac[j] == 'a' || carac[j] == 'e' || carac[j] == 'i' || carac[j] == 'o' || carac[j] == 'u')
+    {
+        
+        Console.Write(carac[j]);
+    }
+}
+Console.WriteLine();
+Console.Write("Consoantes: ");
+for (int j = 0; j < 10; j++)
+{
+    if (carac[j] != 'a' && carac[j] != 'e' && carac[j] != 'i' && carac[j] != 'o' && carac[j] != 'u')
+    {
+        Console.Write(carac[j]);
+    }
+}
